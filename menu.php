0cd104d9d0720614e9a017d8af106cc542b54b27
pangdaxing23 <pangdaxing23@users.noreply.github.com>
2014-07-27 13:10:45 -0400

    thought i already committed this
    it’s the files that are missing

---
+++ menu.php
@@ -0,0 +1,186 @@
+<?php
+session_start();
+if(!$_SESSION['logged']){
+    header("Location: index.php");
+    die;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>eCommerce Project</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap-lumen.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/store-style.css" rel="stylesheet">
+
+    <!-- You can forget about IE8 or lower support. This is the future baby. -->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">eCommerce Project</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <form action="logout.php" class="navbar-form navbar-right" role="search" method="post">
+                    <button type="submit" class="btn btn-default" name="logout">Logout</button>
+                </form>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><?php echo $_SESSION['username'] ?></a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Go to<span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="newPost.php">New Post</a></li>
+                            <li><a href="inventory.php">My Inventory</a></li>
+                            <li><a href="#">My Customers</a></li>
+                            <li class="divider"></li>
+                            <li><a href="viewCart.php">My Cart</a></li>
+                            <li><a href="checkout.php">Checkout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+                <div class="row">
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.it/320x150" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$24.99</h4>
+                                <h4><a href="#">First Product</a>
+                                </h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tempore, quidem quia excepturi deserunt laudantium!</p>
+                            </div>
+
+                            <!-- Probably not going to implement rating system -->
+                           <!--  <div class="ratings">
+                                <p class="pull-right">15 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                </p>
+                            </div> -->
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.it/320x150" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$64.99</h4>
+                                <h4><a href="#">Second Product</a>
+                                </h4>
+                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.it/320x150" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$74.99</h4>
+                                <h4><a href="#">Third Product</a>
+                                </h4>
+                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.it/320x150" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$84.99</h4>
+                                <h4><a href="#">Fourth Product</a>
+                                </h4>
+                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.it/320x150" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">$94.99</h4>
+                                <h4><a href="#">Fifth Product</a>
+                                </h4>
+                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; eCommerce Project 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>eCommerce Project</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap-lumen.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/store-style.css" rel="stylesheet">
+    <link href="css/register.css" rel="stylesheet">
+
+    <!-- You can forget about IE8 or lower support. This is the future baby. -->
+
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">eCommerce Project</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container" id="wrap">
+
+    <?php
+    $message = @$_GET['error'];
+    switch ($message) {
+        case 'fields_empty':
+            echo "<div class=\"alert alert-danger\" role=\"alert\">Some fields were empty, fill out the whole form.</div>";
+            break;
+        case 'unmatching_passwords':
+            echo "<div class=\"alert alert-danger\" role=\"alert\">Passwords didn't match.</div>";
+            break;
+        case 'username_taken':
+            echo "<div class=\"alert alert-danger\" role=\"alert\">Username already taken. Pick another.</div>";
+            break;
+        default:
+            # code...
+            break;
+    }
+        if (@$_GET['registered'] == 'true'){
+                echo "<div class=\"alert alert-success\" role=\"alert\">You have registered successfully.</div>";
+        }
+        ?>
+
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <form action="registration.php" method="post" accept-charset="utf-8" class="form" role="form">
+                    <legend>Register</legend>
+                    <h4>Registration allows you to comment and purchase items.</h4>
+                    <div class="row">
+                        <div class="col-xs-6 col-md-6">
+                            <input type="text" name="firstname" value="" class="form-control input-lg" placeholder="First Name"  />
+                        </div>
+                        <div class="col-xs-6 col-md-6">
+                            <input type="text" name="lastname" value="" class="form-control input-lg" placeholder="Last Name"  />
+                        </div>
+                    </div>
+                    <input type="email" name="email" value="" class="form-control input-lg" placeholder="Your Email" required />
+                    <input type="text" name="username" value="" class="form-control input-lg" placeholder="Your Username" required />
+                    <input type="password" name="password" value="" class="form-control input-lg" placeholder="Password"  />
+                    <input type="password" name="confirm_password" value="" class="form-control input-lg" placeholder="Confirm Password"/>
+                    <button class="btn btn-lg btn-primary btn-block signup-btn" type="submit" name="register">Create my account</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <hr>
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; eCommerce Project 2014</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
